d17bea67e79116b0d26966081bb7b03d70d174c4
buzhidaoya <buzhidaoyaqqqq@gmail.com>
2021-05-01 01:44:25 -0700

    Mokito test cases still have problems and not done yet

--- src/test/java/com/flowers/conniecodetest/controller/JsonControllerTest.java
+++ src/test/java/com/flowers/conniecodetest/controller/JsonControllerTest.java
@@ -1,16 +1,20 @@
 package com.flowers.conniecodetest.controller;
 
 import com.flowers.conniecodetest.model.Entry;
+import com.flowers.conniecodetest.service.EntryService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -29,34 +33,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 public class JsonControllerTest {
 
-    private
-
     @Test
-    void testGetStringjsonToObject() {
-        Iterator i = mock(Iterator.class);
-        when(i.next()).thenReturn("Success!");
-        String res = i.next() + "";
-//        verify(i, times(1)).next();
-        assertEquals("Success!", res);
-    }
-
-    @Test
-    void testJsonObjectObjects() {
-
+    void testJsonObjects() {
+        Entry expectedEntry = new Entry(2, 12,
+                "in quibusdam tempore odit est dolorem",
+                "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente " +
+                        "quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio");
+        List<Entry> list = new ArrayList<>(mock(JsonController.class).jsonToObject());
+        when(list.get(12)).thenReturn(expectedEntry);
+        Entry actualEntry = list.get(12);
+        assertEquals(expectedEntry, actualEntry);
     }
 
 
 
     @Test
-    void countuser() throws Exception {
+    void countuserTest() throws Exception {
+
+    }
+
+    @Test
+    void updateArgumentTest () {
 
     }
 
     @Test
     void updateEntryTest() throws Exception {
-        Entry expectedEntry = new Entry(2, 12, "in quibusdam tempore odit est dolorem",
-                "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet " +
-                        "occaecati quia id voluptatem\nincidunt ea est distinctio odio");
-//        when()
+        Entry expectedEntry = new Entry(2, 12,
+                "in quibusdam tempore odit est dolorem",
+                "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente " +
+                        "quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio");
+        when(mock(JsonController.class).update(12)).thenReturn(expectedEntry);
+        Entry actualEntry = mock(JsonController.class).update(12);
+        assertEquals(expectedEntry, actualEntry);
     }
 }
